00f8c628628dcc30d38070afce0a1ec968ac96c1
AlexK150 <aleksandrstp1@gmail.com>
2020-11-03 10:23:16 +0300

    useState block finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+//useState example
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  function increment() {
+    setCounter(prevState => prevState + 1)
+  }
+
+  function decrement() {
+    setCounter(prevState => prevState - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Счетчик: {counter}</h2>
+      <button onClick={increment} className={'btn btn-success'}>Добавить</button>
+      <button onClick={decrement} className={'btn btn-danger'}>Убрать</button>
     </div>
   );
 }
